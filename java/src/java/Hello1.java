09c6e4698a218b36b444ab42d9c65f4e20df2f58
Sushant1809 <138884395+Sushant1809@users.noreply.github.com>
2023-07-10 10:56:23 +0530

    Added New Class

--- java/src/java/Hello1.java
+++ java/src/java/Hello1.java
@@ -6,6 +6,8 @@ public class Hello1 {
 		// TODO Auto-generated method stub
 		System.out.println("Hello sushant");
 		System.out.println("Hello sushant");
+		System.out.println("Hello sushant");
+
 
 
 	}
